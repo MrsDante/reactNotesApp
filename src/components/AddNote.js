452c03cce36a8e183bcca7f63720b835db95bb88
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-10-28 21:23:54 +0300

    .

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,15 +1,44 @@
-const AddNote = () => {
+import { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState('');
+  //const [charactersLeft, setCharactersLeft] = useState(200);
+  const characterLimit = 200;
+  //let charactersLeft = 0;
+
+  const handleChange = (e) => {
+    //setCharactersLeft(characterLimit - e.target.value.length);
+    if (characterLimit - e.target.value.length >= 0) {
+      setNoteText(e.target.value);
+    } 
+  };
+
+  const handleSaveClick = () => {
+    if (noteText.trim().length === 0) {
+      alert('Пустая заметка? Зачем она нам?');
+    } else {
+      handleAddNote(noteText);
+      setNoteText('');
+    }
+  };
+
     return (
       <div className="note new">
         <textarea 
           rows="8" 
           cols="10" 
-          placeholder="Напиши что-нибудь ;)">
-
+          placeholder="Напиши что-нибудь ;)"
+          onChange={handleChange}
+          value={noteText}
+          >
         </textarea>
         <div className="note-footer">
-            <small>Осталось 200 симолов</small>
-            <button className="save">Сохранить</button>
+            <small>Осталось {characterLimit - noteText.length} симолов</small>
+            <button 
+              className="save"
+              onClick={handleSaveClick}
+            >Сохранить
+            </button>
         </div>
       </div>
     )
